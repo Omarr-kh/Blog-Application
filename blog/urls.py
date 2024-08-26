902f9d79d2690ae0cd9d556e7084f7cff6671766
Omar <omarkhaled1752003@yahoo.com>
2024-08-26 16:26:35 +0300

    url for post deletion

--- blog/urls.py
+++ blog/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path("create-post", views.create_post, name="create-post"),
     path("view-post/<int:post_id>", views.view_post, name="view-post"),
     path("edit-post/<int:post_id>", views.edit_post, name="edit-post"),
+    path("delete-post/<int:post_id>", views.delete_post, name="delete-post"),
 ]
