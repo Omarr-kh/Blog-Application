78f81c0c18c413beb3130c6eaa1dad7aa17ee0dd
Omar <omarkhaled1752003@yahoo.com>
2024-08-26 16:16:18 +0300

    urls for creating, viewing, and editing a post

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("register/", views.register_page, name="register"),
     path("login/", views.login_page, name="login"),
-    path("logout/", views.logoutUser, name="logout"),
+    path("logout/", views.logout_User, name="logout"),
+    path("create-post", views.create_post, name="create-post"),
+    path("view-post/<int:post_id>", views.view_post, name="view-post"),
+    path("edit-post/<int:post_id>", views.edit_post, name="edit-post"),
 ]
