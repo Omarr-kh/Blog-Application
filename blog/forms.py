5fc520489649eed1468ce748fc448644b5fcf572
Omar <omarkhaled1752003@yahoo.com>
2024-08-26 16:15:37 +0300

    form for post creation

--- blog/forms.py
+++ blog/forms.py
@@ -1,9 +1,18 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django import forms
+# from django import forms
+from django.forms import ModelForm
+from .models import Post
 
 
 class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ["username", "email", "password1", "password2"]
+
+
+class PostCreationForm(ModelForm):
+    class Meta:
+        model = Post
+        fields = "__all__"
+        exclude = ["author"]
