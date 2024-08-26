5bfa9a37abcac69728d99f2e341c2298200facf9
Omar <omarkhaled1752003@yahoo.com>
2024-08-26 16:15:20 +0300

    changed how Post model is viewed in admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    # fields to display in the admin list view
+    list_display = ("title", "author", "created_at", "updated_at", "is_published")
+
+    # fields to display in the admin form view
+    fields = ("title", "content", "author", "created_at", "updated_at", "is_published")
+
+    readonly_fields = ("created_at", "updated_at")
+
+
+admin.site.register(Post, PostAdmin)
